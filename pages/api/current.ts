563c61e45c015e6f10c7bb7e495f0ba79f660267
Paras2322 <parasbagga23@Gmail.com>
2023-11-26 19:59:56 +0530

    Netflix clone

---
+++ pages/api/current.ts
@@ -0,0 +1,17 @@
+import serverAuth from "@/lib/serverAuth";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    if (req.method !== 'GET') {
+      return res.status(405).end();
+    }
+
+    const { currentUser } = await serverAuth(req, res);
+
+    return res.status(200).json(currentUser);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).end();
+  }
+}
